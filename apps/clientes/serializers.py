5e54fed2d2c7cc6a311b16fc70a547a12a5cb27e
Thiago Dias Olimpio <thiago.olimpio93@gmail.com>
2024-04-01 16:47:21 -0300

    Validação de campos

--- apps/clientes/serializers.py
+++ apps/clientes/serializers.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
 from apps.clientes.models import Cliente
+from apps.clientes.validators import *
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
+
+    def validate(self, data):
+        
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'cpf':'O CPF deve ter 11 dígitos'})
     
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome':'Não inclua número nesse campo'})
+        
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({'rg':'O CPF deve ter 9 dígitos'})
+        
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular':'O Celular deve ter no minimo 11 dígitos'})
+
+        return data
