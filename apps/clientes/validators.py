d0e22a54dd766168000fb263cb8c82a0088a7589
Thiago Dias Olimpio <thiago.olimpio93@gmail.com>
2024-04-03 17:25:08 -0300

    Nova logica de validação do cpf e celular

--- apps/clientes/validators.py
+++ apps/clientes/validators.py
@@ -1,5 +1,9 @@
+import re
+from validate_docbr import CPF
+
 def cpf_valido(numero_do_cpf):
-    return len(numero_do_cpf) == 11
+    cpf = CPF()
+    return cpf.validate(numero_do_cpf)
 
 def nome_valido(nome):
     return nome.isalpha()
@@ -8,4 +12,13 @@ def rg_valido(rg):
     return len(rg) == 9
 
 def celular_valido(celular):
-    return len(celular) >= 11
+    """
+    Verifica se o celular é valido.
+
+    Modelo: 11 99999-9999
+    """
+
+    modelo = '[0-9]{2} [0-9]{5}-[0-9]{4}'
+
+    return re.findall(modelo, celular)
+
